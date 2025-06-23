0cf1a8fed54531a4d5ef3b7fd0117ccd977459d0
Rafael Terra <rafaelstjf@gmail.com>
2025-06-23 18:51:21 -0300

    first draft on the pycompss workflow

---
+++ src/env_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+module load java/jdk-12_sequana
+module load masa-openmp/1.0.1.1024_sequana
+#module load anaconda3/2024.02_sequana
+#source activate /scratch/cenapadrjsd/rafael.terra2/conda-env
+source /scratch/cenapadrjsd/COMPSs/3.3.3_gnu/compssenv
+
+echo $PYTHONPATH
--- src/main.py
+++ src/main.py
@@ -1,30 +1,36 @@
 from pycompss.api.api import compss_wait_on
 from apps import *
-import argparse, os
+import argparse
+import os
 
 
-def main(alignment_file, work_dir):
-    sequence_dir = create_dir(os.path.join(work_dir, "sequences"))
-    r_split = split_sequences(alignment_file, sequence_dir)
-    r_metrics = list()
-    for i in range (0, len(r_split)):
-        for j in range(i+1, len(r_split)):
-            dir_name = r_split[i].split(".")[-2] + "_" + r_split[j].split(".")[-2]
-            dir = create_dir(os.path.join(work_dir, dir_name))
-            alignment_file = os.path.join(dir, "aligment00.txt")
-            masa(".", "1", os.path.join(sequence_dir, r_split[i]), os.path.join(sequence_dir, r_split[j]),alignment_file)
-            r_metrics.append(get_metrics(alignment_file))
+def main(input_alignment_file, base_work_dir):
+    sequence_dir = create_dir(os.path.join(base_work_dir, "sequences"))
+    split_files_f = split_sequences(input_alignment_file, sequence_dir)
+    split_files = compss_wait_on(split_files_f)
+    pairs_dir = create_dir(os.path.join(base_work_dir, "pair_sequences"))
+    pairs_dir = compss_wait_on(pairs_dir)
+    results = list()
+    for i in range(len(split_files)):
+        for j in range(i + 1, len(split_files)):
+            seq1 = split_files[i]
+            seq2 = split_files[j]
+            pair_id = seq1.split(".")[-2] + "_" + seq2.split(".")[-2]
+            pair_dir = create_dir(os.path.join(pairs_dir, pair_id))
+            align_out="alignment00.txt"
+            results.append(masa_wrapper(pair_dir, 1,seq1, seq2,align_out, os.path.join(base_work_dir, "sequences")))
 
-    compss_wait_on(r_metrics)
-    for m in r_metrics:
-        print(m)
+            #results.append(get_metrics(pair_dir, align_out))
 
-
+    results = compss_wait_on(results)
+    # for res in results:
+    #     print(res)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", type=str, required=True)
-    parser.add_argument("-w","--workdir", type=str, required=False, default=os.getcwd())
+    parser.add_argument("-i", "--input", type=str, required=True, help="Arquivo multifasta de entrada")
+    parser.add_argument("-w", "--workdir", type=str, required=False, default=os.getcwd(), help="Diretório de trabalho")
     args = parser.parse_args()
-    main()
+
+    main(args.input, args.workdir)
