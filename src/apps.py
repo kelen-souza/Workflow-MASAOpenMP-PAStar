056e5db1644894904d1e6d5b6a3818c2046007cf
Rafael Terra <rafaelstjf@gmail.com>
2025-10-06 12:43:58 -0300

    Update apps.py

--- src/apps.py
+++ src/apps.py
@@ -13,7 +13,7 @@ def create_dir(dir):
      return dir
 
 @constraint(computing_units="1")
-@binary(binary="masa-openmp", args="--work-dir {{work_dir}} {{input_1}} {{input_2}}")
+@binary(binary="masa-openmp", args="--alignment-edges=++ --work-dir {{work_dir}} {{input_1}} {{input_2}}")
 @task(work_dir=IN, input_1=FILE_IN, input_2=FILE_IN, alignment_file= INOUT)
 def masa(work_dir, input_1, input_2, alignment_file) :
      """Runs the MASA binary through pycompss
